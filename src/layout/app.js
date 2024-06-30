75f6c563e6f602f2a2c01a7fe5bba195e9e3b808
domyuyi <yucacho2004@gmail.com>
2024-06-30 17:12:25 +0000

    Arreglando el carrusel

--- src/layout/app.js
+++ src/layout/app.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Carrusel from '/workspaces/Carrusel/src/layout/carrusel';
 import '/workspaces/Carrusel/src/css/Carrusel.css';
 
-
 const App = () => {
     return (
         <div>
             <br />
             <center><h2>Procesadores y Tarjeta Grafica</h2></center>
             <br />
-            <Carrusel/>
+            <Carrusel />
         </div>
     );
 };
--- src/layout/single.js
+++ src/layout/single.js
@@ -1,24 +1,31 @@
-import Procesador from '/workspaces/Carrusel/src/procesador.json'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import procesadorData from '/workspaces/Carrusel/src/procesador.json';
 
 const Single = () => {
+  const { slug } = useParams();
+  const procesador = procesadorData.find(it => it.slug === slug);
 
-     const currentRoute = window.location.pathname;
-     const procesador = Procesador.find(it => currentRoute.includes(it.slug))
+  if (!procesador) {
+    return <div>Procesador no encontrado</div>;
+  }
 
-     return (<>
-         <center><h1>{procesador.name}</h1></center>
-           <hr></hr>
-           <div style={{ display: 'flex', justifyContent: 'center' }}>
-           <img 
-              src={procesador.picture} 
-              alt={procesador.name} 
-              style={{ marginBottom: '10px', width: '900px', height: 'auto' }} 
-            />
-            </div>
-           <center><p style={{ marginTop: '10px', fontSize: '16px' }}>{procesador.description}</p></center>
-         
-     </>);
-
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+      <div style={{ flex: '0 0 50%', textAlign: 'center' }}>
+        <img 
+          src={procesador.picture} 
+          alt={procesador.name} 
+          style={{ width: '300px', height: 'auto' }} 
+        />
+      </div>
+      <div style={{ flex: '1', marginLeft: '20px' }}>
+        <h1>{procesador.name}</h1>
+        <p style={{ fontSize: '18px', fontWeight: 'bold' }}>{procesador.price}</p>
+        <p style={{ fontSize: '16px' }}>{procesador.characteristics}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Single;
